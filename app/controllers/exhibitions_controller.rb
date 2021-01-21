04af012485eef411c5123e2623d32482b5ae6236
mt1213123 <mari.orange.tachibana.1996@hotmail.co.jp>
2021-01-21 13:13:06 +0900

    削除機能実装

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,6 +1,6 @@
 class ExhibitionsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
 
   def index
     @exhibition = Exhibition.all
@@ -11,7 +11,6 @@ class ExhibitionsController < ApplicationController
   end
 
   def create
-    binding.pry
     @exhibition = Exhibition.new(exhibition_params)
     if @exhibition.save
       redirect_to root_path
@@ -22,6 +21,14 @@ class ExhibitionsController < ApplicationController
 
   def show
     @exhibition = Exhibition.find(params[:id])
+  end
+
+  def destroy
+    @exhibition = Exhibition.find(params[:id])
+      if current_user.id == @exhibition.user_id
+        @exhibition.destroy
+        redirect_to root_path
+      end
   end
 
   private
