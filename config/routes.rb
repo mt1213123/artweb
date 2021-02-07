478b9587beffdb532042b48c24a7adf9c4962cfa
mt1213123 <mari.orange.tachibana.1996@hotmail.co.jp>
2021-02-07 14:50:33 +0900

    展覧会検索機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: 'exhibitions#index'
   resources :exhibitions, except: [:index] do
     resources :comments, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
 end
